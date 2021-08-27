ab81779f1c90b54686a78670d4c6ed062c968104
Slava Semeniuk <slava.semeniuk@ekreative.com>
2021-08-27 20:10:00 +0300

    decrease timescale

--- video-memory-issue/CMTimeRange+Extension.swift
+++ video-memory-issue/CMTimeRange+Extension.swift
@@ -11,14 +11,22 @@ typealias Seconds = Double
 
 extension CMTimeRange {
     init(start: Seconds, end: Seconds) {
-        self.init(start: CMTime(seconds: start, preferredTimescale: 1_000), end: CMTime(seconds: end, preferredTimescale: 1_000))
+        self.init(start: start.cmTime, end: end.cmTime)
     }
 
     init(start: Seconds, duration: Seconds) {
-        self.init(start: CMTime(seconds: start, preferredTimescale: 1_000), duration: CMTime(seconds: duration, preferredTimescale: 1_000))
+        self.init(start: start.cmTime, duration: duration.cmTime)
     }
 
     func isDisjoint(with timeRange: CMTimeRange) -> Bool {
         intersection(timeRange).isEmpty
     }
 }
+
+extension Seconds {
+    var cmTime: CMTime {
+        CMTime(seconds: self, preferredTimescale: 60)
+    }
+}
+
+
